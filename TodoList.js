7663334d8d2ba2c63015cad527e5b8950ecd17bd
Donovan <DonBond@users.noreply.github.com>
2018-05-04 14:27:01 +0200

    Add files via upload

--- TodoList.js
+++ TodoList.js
@@ -1,43 +1,47 @@
 import React, { Component } from "react";
-    class TodoList extends Component{
-        constructor(props){
-            super(props);
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
-            this.state = {
-                items:[]
+class TodoList extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            items:[]
+        };
+        this.addItem = this.addItem.bind(this);
+    }
+    addItem(e){
+        if(this._inputElement.value !==""){
+            var  newItem = {
+                text: this._inputElement.value,
+                key:Date.now()
             };
-           this.addItem = this.addItem.bind(this);
-        }
-        addItem(e){
-            if(this._inputElement.value !==""){
-                var  newItem = {
-                    text: this._inputElement.value,
-                    key:Date.now()
+
+            this.setState((prevState)=>{
+                return{
+                    items:prevState.items.concat(newItem)
                 };
-
-                this.setState((prevState)=>{
-                    return{
-                        items:prevState.items.concat(newItem)
-                    };
-                });
-                this._inputElement.value="";
-            }
-            console.log(this.state.items);
-            e.preventDefault();
+            });
+            this._inputElement.value="";
         }
-        render(){
-            return(
-                <div className="todoListMain">
-                    <div className="header">
-                        <form onSubmit={this.addItem}>
-                            <input ref={(a)=>this._inputElement = a} placeholder="enter task"/>
-                            <button type="submit">Add Task</button>
-                        </form>
-                    </div>
+        console.log(this.state.items);
+        e.preventDefault();
+    }
+    render(){
+        return(
+            <div className="todoListMain">
+                <div className="header">
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a)=>this._inputElement = a} placeholder="enter task"/>
+                        <button type="submit">Add Task</button>
+                    </form>
                 </div>
-            );
-        }
-
+                <TodoItems entries={this.state.items}/>
+            </div>
+        );
     }
 
-    export default TodoList;
+}
+
+export default TodoList;
